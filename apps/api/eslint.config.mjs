76d94a20952ea056ef3acf52c3ddffc993b952d3
whbbit1999 <luckyboy_dong@sina.com>
2025-10-13 00:13:42 +0800

    feat: api端的初始化

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,27 @@
+import { createConfig } from '@ys/eslint-config/create-config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+
+export default createConfig([
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
+])
