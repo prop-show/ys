9c7f6f15b9005cd107555e50755e8a5d6079aa51
Whbbit <whbbit@qq.com>
2025-11-08 22:48:53 +0800

    fix: 解决postinstall脚本报错的问题

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
 import { visualizer } from 'rollup-plugin-visualizer'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Component from 'unplugin-vue-components/vite'
+// import { VueRouterAutoImports } from 'unplugin-vue-router'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import VueDevtools from 'vite-plugin-vue-devtools'
@@ -19,6 +22,19 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     ui(),
+    // AutoImport({
+    //   include: [/\.[tj]sx?$/, /\.vue$/, /\.md$/],
+    //   imports: ['vue', 'vue-router', VueRouterAutoImports],
+    //   dirs: ['src/composables/**/*.ts', 'src/constants/**/*.ts', 'src/stores/**/*.ts'],
+    //   defaultExportByFilename: true,
+    // }),
+    // Component({
+    //   dirs: ['src/components'],
+    //   collapseSamePrefixes: true, // 组件名去重
+    //   directoryAsNamespace: true, // 以目录名作为命名空间
+    //   // 如果有element或者其他的UI库，可以在这里配置
+    //   // resolvers: [ElementPlusResolver()],
+    // }),
     vueJsx(),
     VueDevtools(),
     visualizer({
